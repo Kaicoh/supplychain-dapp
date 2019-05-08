58a758a86c244b60eae7469644fc3ef089235326
Kaicoh <sumireminami@gmail.com>
2019-05-08 16:21:16 +0900

    add deploying feature

--- truffle.js
+++ truffle.js
@@ -1,4 +1,14 @@
 const path = require('path');
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+require('dotenv').config();
+
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = process.env.METAMASK_MNEMONIC;
+
+if (!infuraKey || !mnemonic) {
+    throw new Error('Not found environment variables. INFURA_KEY, METAMASK_MNEMONIC.');
+}
 
 module.exports = {
     networks: {
@@ -7,6 +17,14 @@ module.exports = {
             port: 7545,
             network_id: '*',
         },
+
+        rinkeby: {
+            provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+            network_id: 4,
+            gas: 4500000,
+            gasPrice: 10000000000,
+        },
+
     },
 
     compilers: {
