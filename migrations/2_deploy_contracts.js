9694b745bc490559347bf50d53b82e9319f0bfcb
Kaicoh <sumireminami@gmail.com>
2019-05-08 16:38:50 +0900

    fix deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,5 @@
-const FarmerRole = artifacts.require('FarmerRole');
-const DistributorRole = artifacts.require('DistributorRole');
-const RetailerRole = artifacts.require('RetailerRole');
-const ConsumerRole = artifacts.require('ConsumerRole');
-const AccessControl = artifacts.require('AccessControl');
-const SupplyChain = artifacts.require('SupplyChain');
-const Ownable = artifacts.require('Ownable');
 const Core = artifacts.require('Core');
 
 module.exports = function (deployer) {
-    deployer.deploy(FarmerRole);
-    deployer.deploy(DistributorRole);
-    deployer.deploy(RetailerRole);
-    deployer.deploy(ConsumerRole);
-    deployer.deploy(AccessControl);
-    deployer.deploy(SupplyChain);
-    deployer.deploy(Ownable);
     deployer.deploy(Core);
 };
